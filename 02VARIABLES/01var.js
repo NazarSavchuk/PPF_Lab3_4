b4a040096d82aa3506f0ac26437962858c2f0e3c
NazarSavchuk <nazarsavcuk3@gmail.com>
2021-12-21 21:13:51 +0200

    message

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,18 @@
+var x = 4;
+var y = 6;
+var z = x + y;
+
+var namr = "Nazar";
+
+var person = "Karolina Kowalska", carName = "Nissan", price = 20000;
+
+var personA = "Karolina Kowalska",
+carNameA = "Nissan",
+priceA = 20000;
+
+var carNameB;
+var personX = "Janina" + " " + "Nowak";
+
+var isFamous = true;
+
+var xx = 2 + 3 + "5";
